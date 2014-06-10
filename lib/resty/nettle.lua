03ce264bb440ad974a432d94d981ae969d67257c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-06-11 02:17:38 +0300

    Added typedef for nettle_cipher_func.

--- lib/resty/nettle.lua
+++ lib/resty/nettle.lua
@@ -1,4 +1,9 @@
 local ffi        = require "ffi"
 local ffi_load   = ffi.load
+local ffi_cdef   = ffi.cdef
+
+ffi_cdef[[
+typedef void nettle_cipher_func(const void *ctx, size_t length, uint8_t *dst, const uint8_t *src);
+]]
 
 return ffi_load("libnettle")
