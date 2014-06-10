7a9faecb046368f8512f31ff020e6b1266564ff6
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-06-10 13:02:27 +0300

    Fixed ffi_str call that was not having len parameter.

--- lib/resty/nettle/pbkdf2.lua
+++ lib/resty/nettle/pbkdf2.lua
@@ -18,13 +18,13 @@ local pbkdf2 = {}
 function pbkdf2.hmac_sha1(key, iterations, salt, len)
     local buf = ffi_new(uint8t, len)
     nettle.nettle_pbkdf2_hmac_sha1(#key, key, iterations, #salt, salt, len, buf)
-    return ffi_str(buf):sub(1, len)
+    return ffi_str(buf, len)
 end
 
 function pbkdf2.hmac_sha256(key, iterations, salt, len)
     local buf = ffi_new(uint8t, len)
     nettle.nettle_pbkdf2_hmac_sha256(#key, key, iterations, #salt, salt, len, buf)
-    return ffi_str(buf):sub(1, len)
+    return ffi_str(buf, len)
 end
 
 return pbkdf2
