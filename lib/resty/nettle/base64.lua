a097052a1f6cae39d036f04c891fc106edad70be
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-06-24 18:47:55 +0300

    Small cleanups to Base64.

--- lib/resty/nettle/base64.lua
+++ lib/resty/nettle/base64.lua
@@ -3,7 +3,6 @@ local ffi_new    = ffi.new
 local ffi_typeof = ffi.typeof
 local ffi_cdef   = ffi.cdef
 local ffi_str    = ffi.string
-local ceil       = math.ceil
 local nettle     = require "resty.nettle"
 
 ffi_cdef[[
@@ -27,15 +26,12 @@ int    nettle_base64_decode_single(struct base64_decode_ctx *ctx, uint8_t *dst, 
 int    nettle_base64_decode_update(struct base64_decode_ctx *ctx, size_t *dst_length, uint8_t *dst, size_t src_length, const uint8_t *src);
 int    nettle_base64_decode_final (struct base64_decode_ctx *ctx);
 ]]
-local ctxenc = ffi_typeof("BASE64_ENCODE_CTX[1]")
+--local ctxenc = ffi_typeof("BASE64_ENCODE_CTX[1]")
 local ctxdec = ffi_typeof("BASE64_DECODE_CTX[1]")
 
 local length = ffi_new("size_t[1]")
 local uint8t = ffi_typeof("uint8_t[?]")
 local base64 = {}
-
---(len * 8 + 4) / 6
---((len + 2)/3)*4
 
 function base64.encode(src)
     local len = #src
@@ -48,10 +44,10 @@ end
 function base64.decode(src)
     local ctx = ffi_new(ctxdec)
     local len = #src
-    local dst = ffi_new(uint8t, ceil((len + 1) * 6 / 8))
+    local dst = ffi_new(uint8t, (len + 1) * 6 / 8)
     nettle.nettle_base64_decode_init(ctx)
     nettle.nettle_base64_decode_update(ctx, length, dst, len, src)
-    nettle.nettle_base64_decode_final(ctx)
+    assert(nettle.nettle_base64_decode_final(ctx) == 1, "Base64 final padding is incorrect.")
     return ffi_str(dst, length[0])
 end
 
