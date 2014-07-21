fc611428057a2101da31c67200866ea309af50e5
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-07-21 23:19:24 +0300

    Added first implementation of the bindings of Yarrow random generator.

---
+++ lib/resty/nettle/yarrow.lua
@@ -0,0 +1,73 @@
+require "resty.nettle.types.aes"
+require "resty.nettle.types.sha2"
+
+local ffi        = require "ffi"
+local ffi_new    = ffi.new
+local ffi_typeof = ffi.typeof
+local ffi_cdef   = ffi.cdef
+local ffi_str    = ffi.string
+local nettle     = require "resty.nettle"
+
+ffi_cdef[[
+enum yarrow_pool_id { YARROW_FAST = 0, YARROW_SLOW = 1 };
+typedef struct yarrow_source {
+  uint32_t estimate[2];
+  enum yarrow_pool_id next;
+} YARROW_SOURCE;
+typedef struct yarrow256_ctx {
+  struct sha256_ctx pools[2];
+  int seeded;
+  struct aes256_ctx key;
+  uint8_t counter[16];
+  unsigned nsources;
+  struct yarrow_source *sources;
+} YARROW256_CTX;
+void nettle_yarrow256_init(struct yarrow256_ctx *ctx, unsigned nsources, struct yarrow_source *sources);
+void nettle_yarrow256_seed(struct yarrow256_ctx *ctx, size_t length, const uint8_t *seed_file);
+int  nettle_yarrow256_update(struct yarrow256_ctx *ctx, unsigned source, unsigned entropy, size_t length, const uint8_t *data);
+void nettle_yarrow256_random(struct yarrow256_ctx *ctx, size_t length, uint8_t *dst);
+int  nettle_yarrow256_is_seeded(struct yarrow256_ctx *ctx);
+unsigned nettle_yarrow256_needed_sources(struct yarrow256_ctx *ctx);
+void nettle_yarrow256_fast_reseed(struct yarrow256_ctx *ctx);
+void nettle_yarrow256_slow_reseed(struct yarrow256_ctx *ctx);
+]]
+
+local uint8t = ffi_typeof("uint8_t[?]")
+local ctx256 = ffi_typeof("YARROW256_CTX[1]")
+
+local yarrow = {}
+yarrow.__index = function(t, k)
+    if k == "seeded" then
+        return nettle.nettle_yarrow256_is_seeded(t.context) == 1
+    elseif k == "sources" then
+        return nettle.nettle_yarrow256_needed_sources(t.context)
+    end
+    return rawget(getmetatable(t), k)
+end
+
+function yarrow.new()
+    local self = setmetatable({ context = ffi_new(ctx256) }, yarrow)
+    nettle.nettle_yarrow256_init(self.context, 0, nil)
+    return self
+end
+
+function yarrow:seed(data)
+    assert(#data > 31, "Seed data length should be at least 32 bytes, but it can be larger.")
+    nettle.nettle_yarrow256_seed(self.context, #data, data)
+end
+
+function yarrow:reseed(fast)
+    fast = fast ~= false
+    if fast then
+        return nettle.nettle_yarrow256_fast_reseed(self.context)
+    end
+    return nettle.nettle_yarrow256_slow_reseed(self.context)
+end
+
+function yarrow:random(length)
+    local buffer = ffi_new(uint8t, length)
+    nettle.nettle_yarrow256_random(self.context, length, buffer)
+    return ffi_str(buffer, length)
+end
+
+return yarrow
