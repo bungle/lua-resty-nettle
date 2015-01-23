c0b19d9dc0d734cc9d592915d53ce5a21fcf94a9
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-01-23 16:25:11 +0200

    Added knuth-lfib.

---
+++ lib/resty/nettle/knuth-lfib.lua
@@ -0,0 +1,50 @@
+local ffi        = require "ffi"
+local ffi_new    = ffi.new
+local ffi_typeof = ffi.typeof
+local ffi_cdef   = ffi.cdef
+local ffi_str    = ffi.string
+local nettle     = require "resty.nettle"
+
+ffi_cdef[[
+typedef struct knuth_lfib_ctx {
+  uint32_t x[100];
+  unsigned index;
+} KNUTH_LFIB_CTX;
+void     nettle_knuth_lfib_init(struct knuth_lfib_ctx *ctx, uint32_t seed);
+uint32_t nettle_knuth_lfib_get(struct knuth_lfib_ctx *ctx);
+void     nettle_knuth_lfib_get_array(struct knuth_lfib_ctx *ctx, size_t n, uint32_t *a);
+void     nettle_knuth_lfib_random(struct knuth_lfib_ctx *ctx, size_t n, uint8_t *dst);
+]]
+
+local uint8t  = ffi_typeof("uint8_t[?]")
+local uint32t = ffi_typeof("uint32_t[?]")
+local ctx = ffi_typeof("KNUTH_LFIB_CTX[1]")
+
+local knuth = {}
+knuth.__index = knuth
+
+function knuth.new(seed)
+    local self = setmetatable({ context = ffi_new(ctx) }, knuth)
+    nettle.nettle_knuth_lfib_init(self.context, seed or 0)
+    return self
+end
+
+function knuth:number()
+    return nettle.nettle_knuth_lfib_get(self.context)
+end
+
+function knuth:array(n)
+    local b = ffi_new(uint32t, n)
+    nettle.nettle_knuth_lfib_get_array(self.context, n, b)
+    local r = {}
+    for i=1, n do r[i] = b[i-1] end
+    return r
+end
+
+function knuth:random(n)
+    local b = ffi_new(uint8t, n)
+    nettle.nettle_knuth_lfib_random(self.context, n, b)
+    return ffi_str(b, n)
+end
+
+return knuth
