8a15c37748ce94f7f3b880e3a47b92700d550fdc
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-07-01 16:50:14 +0300

    Added some EAX mode function cdefs.

--- lib/resty/nettle/types/eax.lua
+++ lib/resty/nettle/types/eax.lua
@@ -14,4 +14,10 @@ typedef struct eax_ctx {
   union nettle_block16 omac_message;
   union nettle_block16 ctr;
 } EAX_CTX;
+void nettle_eax_set_key (struct eax_key *key, const void *cipher, nettle_cipher_func *f);
+void nettle_eax_set_nonce (struct eax_ctx *eax, const struct eax_key *key, const void *cipher, nettle_cipher_func *f, size_t nonce_length, const uint8_t *nonce);
+void nettle_eax_update (struct eax_ctx *eax, const struct eax_key *key, const void *cipher, nettle_cipher_func *f, size_t data_length, const uint8_t *data);
+void nettle_eax_encrypt (struct eax_ctx *eax, const struct eax_key *key, const void *cipher, nettle_cipher_func *f, size_t length, uint8_t *dst, const uint8_t *src);
+void nettle_eax_decrypt (struct eax_ctx *eax, const struct eax_key *key, const void *cipher, nettle_cipher_func *f, size_t length, uint8_t *dst, const uint8_t *src);
+void nettle_eax_digest (struct eax_ctx *eax, const struct eax_key *key, const void *cipher, nettle_cipher_func *f, size_t length, uint8_t *digest);
 ]]
