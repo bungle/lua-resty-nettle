13bd26ca731600d725e74eef2c2e10d0ec4ee707
Shashi Ranjan <shashi@mashape.com>
2017-09-27 09:30:12 -0700

    add option to load lib from global lib path

--- lib/resty/nettle/gmp.lua
+++ lib/resty/nettle/gmp.lua
@@ -6,8 +6,12 @@ local pcall    = pcall
 local function L()
     local ok, lib = pcall(ffi_load, "gmp")
     if ok and lib then return lib end
+
     for _, t in ipairs{ "so", "dylib", "dll" } do
         ok, lib = pcall(ffi_load, "gmp." .. t)
+        if ok and lib then return lib end
+        local lib_path = (_GMP_LIB_PATH or "") .. "libgmp." .. t
+        ok, lib = pcall(ffi_load, lib_path)
         if ok and lib then return lib end
         for i = 10, 3, -1 do
             ok, lib = pcall(ffi_load, "gmp." .. i)
@@ -15,6 +19,8 @@ local function L()
             ok, lib = pcall(ffi_load, "gmp." .. t .. "." .. i)
             if ok and lib then return lib end
             ok, lib = pcall(ffi_load, "libgmp." .. t .. "." .. i)
+            if ok and lib then return lib end
+            ok, lib = pcall(ffi_load, lib_path .. "." .. i)
             if ok and lib then return lib end
         end
     end
