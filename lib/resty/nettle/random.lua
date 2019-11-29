6f510c64a034d797cb1632489b782270b1d73aaa
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-11-29 17:51:40 +0200

    fix(random) fix a leak of callback resulting "too many callbacks" error

--- lib/resty/nettle/random.lua
+++ lib/resty/nettle/random.lua
@@ -1,32 +1,40 @@
 if ngx then
+  require "resty.nettle.types.nettle-types"
+
   local random = require "resty.random"
 
   local ffi = require "ffi"
   local C = ffi.C
 
+  local func = ffi.cast("nettle_random_func *", function(_, length, dst)
+    C.RAND_bytes(dst, length)
+  end)
+
   return {
     bytes = random.bytes,
     context = nil,
-    func = function(_, length, dst)
-      C.RAND_bytes(dst, length)
-    end
+    func = func,
   }
 end
 
-local knuth = require "resty.nettle.knuth-lfib"
+do
+  local os = os
+  local math = math
+  local knuth = require "resty.nettle.knuth-lfib"
 
-math.randomseed(os.clock() * 1000000 * os.time())
+  math.randomseed(os.clock() * 1000000 * os.time())
 
-math.random()
-math.random()
-math.random()
+  math.random()
+  math.random()
+  math.random()
 
-local k = knuth.new(math.random() * 100000000000000)
+  local k = knuth.new(math.random() * 100000000000000)
 
-return {
-  bytes = function(n)
-    return k:random(n)
-  end,
-  context = k.context,
-  func = knuth.func,
-}
+  return {
+    bytes = function(n)
+      return k:random(n)
+    end,
+    context = k.context,
+    func = knuth.func,
+  }
+end
